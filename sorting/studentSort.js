92c89095d5c44d96fe94f97749f9b58e44fe78e6
MikeEngerer <michael.engerer@hotmail.com>
2018-11-29 17:28:40 +0000

    correction to student sort

--- sorting/studentSort.js
+++ sorting/studentSort.js
@@ -12,7 +12,7 @@ students.sort(function(student1, student2) {
 	if (student1.name < student2.name) {
 		return -1
 	}
-	if (student1.name > student2.name) {
+	if (student1.name === student2.name) {
 		if (student1.age < student2.age) {
 			return -1;
 		}
